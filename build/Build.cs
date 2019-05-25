9a30049668ba06224b05ff27c58cbc1adc62de44
Andrew Stakhov <andrew.stakhov@gmail.com>
2019-05-25 15:58:14 -0400

    Push build task

--- build/Build.cs
+++ build/Build.cs
@@ -9,6 +9,7 @@ using Nuke.Common.Execution;
 using Nuke.Common.Git;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.CloudFoundry;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.Git;
 using Nuke.Common.Tools.GitVersion;
@@ -18,6 +19,7 @@ using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Nuke.Common.Tools.CloudFoundry.CloudFoundryTasks;
 
 [CheckBuildProjectConfigurations]
 [UnsetVisualStudioEnvironmentVariables]
@@ -39,6 +41,20 @@ class Build : NukeBuild
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
     [GitVersion] readonly GitVersion GitVersion;
+    [Parameter("Cloud Foundry Username")]
+    readonly string CfUsername;
+    [Parameter("Cloud Foundry Password")]
+    readonly string CfPassword;
+    [Parameter("Cloud Foundry Endpoint")]
+    readonly string CfApiEndpoint;
+    [Parameter("Cloud Foundry Org")]
+    readonly string CfOrg;
+    [Parameter("Cloud Foundry Space")]
+    readonly string CfSpace;
+
+    [Parameter("Skip logging in Cloud Foundry and use the current logged in session")] 
+    readonly bool CfSkipLogin;
+
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath ArtifactsDirectory => (AbsolutePath)TeamServices.Instance?.ArtifactStagingDirectory ??  RootDirectory / "artifacts";
@@ -93,6 +109,44 @@ class Build : NukeBuild
             ZipFile.CreateFromDirectory(PublishDirectory, ArtifactsDirectory / PackageZipName);
             Logger.Block(ArtifactsDirectory / PackageZipName);
         });
+
+    Target CfLogin => _ => _
+        .OnlyWhenStatic(() => !CfSkipLogin)
+        .Requires(() => CfUsername, () => CfPassword, () => CfApiEndpoint)
+        
+        .Executes(() =>
+        {
+            CloudFoundryLogin(c => c
+                .SetUsername(CfUsername)
+                .SetPassword(CfPassword)
+                .SetApiEndpoint(CfApiEndpoint));
+        });
+    Target CfPush => _ => _
+        .DependsOn(CfLogin)
+        .Requires(() => CfSpace, () => CfOrg)
+        .Executes(async () =>
+        {
+            string appName = "ers1";
+            CloudFoundryTarget(c => c
+                .SetSpace(CfSpace)
+                .SetOrg(CfOrg));
+            CloudFoundryCreateService(c => c
+                .SetService("p-service-registry")
+                .SetPlan("standard")
+                .SetInstanceName("eureka"));
+            CloudFoundryPush(c => c
+                .SetAppName(appName)
+                .SetRandomRoute(true)
+                .SetPath(ArtifactsDirectory / PackageZipName));
+            await CloudFoundryEnsureServiceReady("eureka");
+            CloudFoundryBindService(c => c
+                .SetAppName(appName)
+                .SetServiceInstance("eureka"));
+            CloudFoundryRestart(c => c
+                .SetAppName(appName));
+        });
+    
+    
     
     Target Release => _ => _
         .Description("Creates a GitHub release (or ammends existing) and uploads buildpack artifact")
@@ -141,6 +195,8 @@ class Build : NukeBuild
             
             Logger.Block(releaseAsset.BrowserDownloadUrl);
         });
+    
+    
     
     bool IsGitPushedToRemote => GitTasks
         .Git("status")
